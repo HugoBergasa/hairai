7c687c242b56c585176296f5563f7bf14cef9187
Hugo <hbergasa@gmail.com>
2025-09-04 18:43:25 +0200

    Initial commit

---
+++ src/main/java/com/peluqueria/recepcionista_virtual/RecepcionistaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.peluqueria.recepcionista_virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepcionistaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepcionistaVirtualApplication.class, args);
+	}
+
+}
